1f7224e7d5d07ce309b4e9eaf7062180e1c0ec8e
Golliard <golliard.info@gmail.com>
2020-03-31 23:12:16 +0200

    31-03-2020

---
+++ src/_data/settings.js
@@ -0,0 +1,12 @@
+module.exports = {
+    societe: "Dédoc SA",
+    adresse: "route de Tabary 1",
+    ville: "1904 Vernayaz",
+    url: "https://www.dedoc.ch",
+    Email: "info@dedoc.ch",
+    telephone: "058 911 04 40",
+sociale: [          "<a href='#' class='fa fa-facebook'></a>",
+                    "<a href='#' class='fa fa-twitter'></a>",
+                    "<a href='#' class='fa fa-linkedin'></a>",
+                    "<a href='#' class='fa fa-pinterest'></a>"]
+  };
