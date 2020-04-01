a3ad4d21c67a64df818536b563a3bd8c08dc35ed
Golliard <golliard.info@gmail.com>
2020-04-01 14:20:42 +0200

    ajouter gitignore

--- src/_data/settings.js
+++ src/_data/settings.js
@@ -5,8 +5,8 @@ module.exports = {
     url: "https://www.dedoc.ch",
     Email: "info@dedoc.ch",
     telephone: "058 911 04 40",
-    sociale: [      "<a href='#' class='fa fa-facebook'></a>",
-                    "<a href='#' class='fa fa-twitter'></a>",
-                    "<a href='#' class='fa fa-linkedin'></a>",
-                    "<a href='#' class='fa fa-pinterest'></a>"]
+    sociale: [      "<a href='https://facebook.com' class='fa fa-facebook'></a>",
+                    "<a href='https://twitter.com' class='fa fa-twitter'></a>",
+                    "<a href='https://linkedin.com' class='fa fa-linkedin'></a>",
+                    "<a href='httpss://pinterest.com' class='fa fa-pinterest'></a>"]
   };
